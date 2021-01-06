3c762033f7e0e6cfc377f2cebe1a0ec941d9cb99
Natchapol Srisang <utopiabeam@gmail.com>
2021-01-06 11:28:24 +0700

    feat(user): add user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly service: UserService) {}
+
+  @Get(':id')
+  findById(@Param('id', new ParseIntPipe()) id: number): Promise<User> {
+    return this.service.findById(id);
+  }
+
+  @Post()
+  create(@Body() dto: Omit<User, 'id'>): Promise<User> {
+    return this.service.create(dto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', new ParseIntPipe()) id: number,
+    @Body() dto: Partial<Omit<User, 'id'>>,
+  ): Promise<User> {
+    return this.service.update(id, dto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', new ParseIntPipe()) id: number): Promise<User> {
+    return this.service.delete(id);
+  }
+}
