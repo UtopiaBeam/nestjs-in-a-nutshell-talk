48fd7b0bed56fcb9f0477d2e71ce66f9b9bbd5a1
Natchapol Srisang <utopiabeam@gmail.com>
2021-01-06 11:13:32 +0700

    feat(user): implement user service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly repo: Repository<User>,
+  ) {}
+
+  findById(id: number): Promise<User> {
+    return this.repo.findOne(id);
+  }
+
+  create(dto: Omit<User, 'id'>): Promise<User> {
+    const user = { ...new User(), ...dto };
+    return this.repo.save(user);
+  }
+
+  async update(id: number, dto: Partial<Omit<User, 'id'>>): Promise<User> {
+    const user = { ...(await this.findById(id)), ...dto };
+    return this.repo.save(user);
+  }
+
+  async delete(id: number) {
+    const user = await this.findById(id);
+    await this.repo.remove(user);
+    return user;
+  }
+}
