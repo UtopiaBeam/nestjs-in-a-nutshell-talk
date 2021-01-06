5a41de44362cb41ba464fb66f394b5b0b6f82155
Natchapol Srisang <utopiabeam@gmail.com>
2021-01-06 12:54:25 +0700

    feat(user): hash password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
-
+import { hash } from 'bcryptjs';
 @Injectable()
 export class UserService {
   constructor(
@@ -13,13 +13,21 @@ export class UserService {
     return this.repo.findOne(id);
   }
 
-  create(dto: Omit<User, 'id'>): Promise<User> {
-    const user = { ...new User(), ...dto };
+  findByUsername(username: string): Promise<User> {
+    return this.repo.findOne({ username });
+  }
+
+  async create(dto: Omit<User, 'id'>): Promise<User> {
+    const password = await hash(dto.password, 10);
+    const user = { ...new User(), ...dto, password };
     return this.repo.save(user);
   }
 
   async update(id: number, dto: Partial<Omit<User, 'id'>>): Promise<User> {
     const user = { ...(await this.findById(id)), ...dto };
+    if (dto.password) {
+      user.password = await hash(dto.password, 10);
+    }
     return this.repo.save(user);
   }
 
