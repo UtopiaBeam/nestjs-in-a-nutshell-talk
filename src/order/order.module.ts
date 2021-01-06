a35125722768e1ab9fce2e2092cedb4a7eaa0f97
Natchapol Srisang <utopiabeam@gmail.com>
2021-01-06 12:13:05 +0700

    refactor(order): remove unused import

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { OrderItem } from 'src/entities/order-item.entity';
 import { Order } from 'src/entities/order.entity';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
