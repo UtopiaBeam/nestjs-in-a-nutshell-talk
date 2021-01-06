08379f1535a5909e59de32ddc8718ca3ccc9df81
Natchapol Srisang <utopiabeam@gmail.com>
2021-01-06 15:36:14 +0700

    feat(auth): add auth guard

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,10 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const req = context.switchToHttp().getRequest<Request>();
+    return req.uid !== undefined;
+  }
+}
